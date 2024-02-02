078b0c09ccc9c2d716b28d2c07fe5c6ae0a604de
0xhawk <hawk@horizonx.tech>
2024-02-02 15:19:05 +0400

    usdc: tx filter for ERC-20 Transfer

--- src/logics/usdc_daily_tx_filter/src/lib.rs
+++ src/logics/usdc_daily_tx_filter/src/lib.rs
@@ -1,15 +1,1 @@
-use usdc_daily_tx_filter_accessors::*;
-#[derive(Clone, Debug, Default, candid :: CandidType, serde :: Deserialize, serde :: Serialize)]
-pub struct LensValue {
-    pub dummy: u64,
-}
-pub type CalculateArgs = (u64, u64);
-
-pub async fn calculate(targets: Vec<String>, args: CalculateArgs) -> LensValue {
-    let results =
-        get_events_from_to_in_usdc_transfer_indexer(targets.get(0usize).unwrap().clone(), args)
-            .await
-            .unwrap();
-
-    
-}
+use usdc_daily_tx_filter_accessors :: * ; # [derive (Clone , Debug , Default , candid :: CandidType , serde :: Deserialize , serde :: Serialize)] pub struct LensValue { pub dummy : u64 } # [derive (Clone , Debug , Default , candid :: CandidType , serde :: Deserialize , serde :: Serialize)] pub struct CalculateArgs { pub dummy : u64 } pub async fn calculate (targets : Vec < String > , args : CalculateArgs) -> LensValue { let _result = get_events_from_to_in_usdc_transfer_indexer (targets . get (0usize) . unwrap () . clone () , todo ! ("Arguments to be used in this call")) . await ; todo ! () }
